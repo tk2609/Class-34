e6824226d8e76319e6731ff228ff12b31a003203
tk2609 <uma.kosaraju@gmail.com>
2020-05-21 19:48:03 +0530

    display

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-var ball;
+var ball, database, position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value",readPosition, showError);
+
 }
 
 function draw(){
@@ -24,6 +28,18 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x': position.x+x,
+        'y': position.y+y,
+
+    })  
+}
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y
+}
+function showError(){
+    console.log("error in writing inside the database");
+    
 }
